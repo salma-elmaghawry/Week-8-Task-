f06c59e32c1224166d3e9977e9b51ce1dc343b2c
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-05-12 19:58:50 +0300

    Task 2.1 Done

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:week8/BMI/bmi_screen.dart';
-import 'package:week8/Gallery/gallery_screen.dart';
-import 'package:week8/Notes/notes_screen.dart';
+import 'package:week8/core/app_colors.dart';
+import 'package:week8/feature/BMI/bmi_screen.dart';
+import 'package:week8/feature/Gallery/gallery_screen.dart';
+import 'package:week8/feature/Notes/notes_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -13,11 +14,7 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = [
-    const BmiScreen(),
-    const GalleryScreen(),
-    const NotesScreen(),
-  ];
+  final List<Widget> _screens = [BmiScreen(), GalleryScreen(), NotesScreen()];
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +22,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       body: IndexedStack(index: _currentIndex, children: _screens),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.deepPurple,
+        selectedItemColor: AppColors.primary,
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
         currentIndex: _currentIndex,
